ff4aa1f54af2e991c6413af8215acabc281f255f
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-06 18:28:15 -0300

    creo navbar y routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: '/home', pathMatch: 'full'},
+    {path:'home', component: HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'login', component: LoginComponent},
+    {path: '**', component: ErrorComponent},
+
+];
