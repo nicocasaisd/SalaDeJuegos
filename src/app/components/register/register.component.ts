01a92a00132ccb03027bfee2c7f366dffc72acea
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-17 16:14:37 -0300

    componente de register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  constructor(
+    private auth: Auth,
+    private firestore: Firestore,
+    public authService: AuthService,
+    private router: Router
+  ) {}
+
+  
+  userMail: string = '';
+  userPass: string = '';
+
+  register(){
+
+  }
 }
