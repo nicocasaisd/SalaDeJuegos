717c1523eac3250fa210252ff84f7395df92a33d
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-17 17:38:11 -0300

    cambio el boton de cerrar sesion de usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Auth, signOut } from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  public loggedUser: string = '';
 
-  public loggedUser : string = "";
-
-  constructor() { }
+  constructor(private auth: Auth) {}
 
   setLoggedUser(value: string) {
     this.loggedUser = value;
@@ -16,4 +16,17 @@ export class AuthService {
   getLoggedUser() {
     return this.loggedUser;
   }
+
+  logOut() {
+    this.loggedUser = '';
+    this.closeSession();
+  }
+
+  closeSession() {
+    console.log('Cerrando la sesión de ', this.auth.currentUser?.email);
+    signOut(this.auth).then(() => {
+      console.log(this.auth.currentUser?.email);
+    });
+  }
+
 }
