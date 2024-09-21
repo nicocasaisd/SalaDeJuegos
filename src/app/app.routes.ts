946de3bf1556f765c34cd14ecc4d19722f0c7c9a
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-21 15:28:38 -0300

    creo chat service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './components/about/about.component';
 import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 export const routes: Routes = [
     {path:'', redirectTo: '/home', pathMatch: 'full'},
@@ -11,6 +12,7 @@ export const routes: Routes = [
     {path:'about', component: AboutComponent},
     {path:'login', component: LoginComponent},
     {path:'register', component: RegisterComponent},
+    {path:'chat', component: ChatComponent},
     {path: '**', component: ErrorComponent},
 
 ];
