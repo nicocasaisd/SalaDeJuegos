7921221af84d518a4bf5b3ec44d216f02c7ef90e
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-21 21:36:10 -0300

    avanzando con chat component

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatMessage, ChatService } from '../../services/chat.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit{
+  messages: ChatMessage[] = [];
+  newMessage: string = '';
+  sender: string = 'Ninguno';
 
+  constructor(private chatService : ChatService, private auth : AuthService){}
+  
+  ngOnInit(): void {
+    // Inicializo sender
+    this.sender = this.auth.getLoggedUser();
+    const temp = this.chatService.getMessages();
+    console.log('Temp: ', temp);
+    // Llamo al chat service
+    temp.subscribe( (messages) => {
+      this.messages = messages;
+      console.log('Messages: ', messages);
+    })
+  }
+
+  // Ver
+  sendMessage(): void {
+    if (this.newMessage.trim()) {
+      this.chatService.sendMessage(this.newMessage, this.sender).then(() => {
+        this.newMessage = ''; // Clear input after sending
+      });
+    }
+  }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { addDoc, collection, collectionData, Firestore, Timestamp } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface ChatMessage{
+  message: string;
+  sender: string;
+  timestamp: Timestamp;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class ChatService {
 
-  constructor() { }
+  constructor(private firestore : Firestore) { }
+
+  // Obtener mensajes
+  getMessages() : Observable<ChatMessage[]>{
+    const messages =  collection(this.firestore, 'mensajes');
+    
+    return collectionData(messages, {idFIeld: 'id'}) as Observable<ChatMessage[]>;
+  }
+
+  // Enviar mensajes
+  sendMessage(message: string, sender: string) : Promise<void> {
+    const messages = collection(this.firestore, 'mensajes');
+
+    return addDoc(messages, {
+      message,
+      sender,
+      timestamp: Timestamp.now(),
+      date: Date.now()
+    }).then(() => {
+      console.log("Message sent successfully");
+    }).catch((error) => {
+      console.error("Error sending message: ", error);
+    });
+
+  }
+
 }
