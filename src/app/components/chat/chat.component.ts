b1016998cce5e2ff3a7e73663c2741b26069d3f1
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-21 22:39:05 -0300

    scrollTo Bottom automatico

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ChatMessage, ChatService } from '../../services/chat.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -12,7 +13,9 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './chat.component.css'
 })
 export class ChatComponent implements OnInit{
-  messages: ChatMessage[] = [];
+  @ViewChild('messagesContainer') private messagesContainer!: ElementRef;
+  //messages: ChatMessage[] = [];
+  messages!: Observable<ChatMessage[]>;
   newMessage: string = '';
   sender: string = 'Ninguno';
 
@@ -21,12 +24,14 @@ export class ChatComponent implements OnInit{
   ngOnInit(): void {
     // Inicializo sender
     //this.sender = this.auth.getLoggedUser();
-    const temp = this.chatService.getMessages();
-    console.log('Temp: ', temp);
+    //const temp = this.chatService.getMessages();
+    this.messages = this.chatService.getMessages();
+    //console.log('Temp: ', temp);
     // Llamo al chat service
-    temp.subscribe( (messages) => {
-      this.messages = messages;
-      console.log('Messages: ', messages);
+    this.messages.subscribe( () => {
+      //this.messages = messages;
+      //console.log('Messages: ', messages);
+      
     })
   }
 
@@ -34,8 +39,19 @@ export class ChatComponent implements OnInit{
   sendMessage(): void {
     if (this.newMessage.trim()) {
       this.chatService.sendMessage(this.newMessage, this.sender).then(() => {
-        this.newMessage = ''; // Clear input after sending
+        
       });
     }
+    this.newMessage = ''; // Clear input after sending
+  }
+
+  ngAfterViewChecked() {
+    setTimeout(()=>this.scrollToBottom(), 100);
+    
+  }
+
+  private scrollToBottom(): void {
+    const container = this.messagesContainer.nativeElement;
+    container.scrollTop = container.scrollHeight;
   }
 }
