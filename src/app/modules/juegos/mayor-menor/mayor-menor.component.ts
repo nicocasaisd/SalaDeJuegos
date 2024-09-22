ca3fb44f93fab5ba5206bd7b0e1c29ceaedc9af0
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-22 18:06:34 -0300

    avanzo en logica mayor-menor

--- src/app/modules/juegos/mayor-menor/mayor-menor.component.ts
+++ src/app/modules/juegos/mayor-menor/mayor-menor.component.ts
@@ -2,6 +2,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BtnSalirComponent } from "../btn-salir/btn-salir.component";
 import { JuegosService } from '../../../services/juegos.service';
 
+interface Carta {
+  numero : number;
+  palo : string;
+}
+
+
 @Component({
   selector: 'app-mayor-menor',
   standalone: true,
@@ -9,11 +15,15 @@ import { JuegosService } from '../../../services/juegos.service';
   templateUrl: './mayor-menor.component.html',
   styleUrl: './mayor-menor.component.css'
 })
+
 export class MayorMenorComponent implements OnDestroy, OnInit {
   
   constructor(public juego: JuegosService){
-
+    this.generateDeck();
   }
+  
+
+  mazo: Carta[] = [];
 
   ngOnInit(): void {
     this.juego.setModuleLoaded(true);
@@ -25,4 +35,14 @@ export class MayorMenorComponent implements OnDestroy, OnInit {
     this.juego.setModuleLoaded(false);
     console.log('Se destruye el componente');
   }
+
+  generateDeck() {
+    const palos = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+    const numeros = [2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+    for (const palo of palos) {
+      for (const numero of numeros) {
+        this.mazo.push({ numero, palo });
+      }
+    }
 }
