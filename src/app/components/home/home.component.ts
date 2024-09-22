4aab45a46411708f36fc70eeb30f1f6bac2a36a6
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-22 11:17:11 -0300

    creo componentes de juegos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
