caf08bd671b1d4dc9dcc62de495daab7a413bf32
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-22 21:26:07 -0300

    avance

--- src/app/modules/juegos/mayor-menor/mayor-menor.component.ts
+++ src/app/modules/juegos/mayor-menor/mayor-menor.component.ts
@@ -1,48 +1,73 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { BtnSalirComponent } from "../btn-salir/btn-salir.component";
+import { BtnSalirComponent } from '../btn-salir/btn-salir.component';
 import { JuegosService } from '../../../services/juegos.service';
 
-interface Carta {
-  numero : number;
-  palo : string;
+interface Card {
+  numero: number;
+  palo: string;
 }
-
 
 @Component({
   selector: 'app-mayor-menor',
   standalone: true,
   imports: [BtnSalirComponent],
   templateUrl: './mayor-menor.component.html',
-  styleUrl: './mayor-menor.component.css'
+  styleUrl: './mayor-menor.component.css',
 })
-
 export class MayorMenorComponent implements OnDestroy, OnInit {
   
-  constructor(public juego: JuegosService){
+  constructor(public juego: JuegosService) {
     this.generateDeck();
+    this.mezclarMazo();
+    console.log(this.mazo);
+    this.currentCard = this.mazo.pop();
+    this.nextCard = this.mazo.pop();
+    console.log(this.currentCard, this.nextCard);
+    this.currentCardImage = this.getCardImage(this.currentCard);
   }
-  
 
-  mazo: Carta[] = [];
+  mazo: Card[] = [];
+
+  currentCard: any;
+  nextCard: any;
+  currentCardImage : string = '';
 
   ngOnInit(): void {
     this.juego.setModuleLoaded(true);
     console.log('Se crea el componente');
   }
-  
-  
+
   ngOnDestroy(): void {
     this.juego.setModuleLoaded(false);
     console.log('Se destruye el componente');
   }
 
   generateDeck() {
-    const palos = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-    const numeros = [2,3,4,5,6,7,8,9,10,11,12,13,14];
+    const palos = ['hearts', 'spades'];
+    const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
     for (const palo of palos) {
       for (const numero of numeros) {
         this.mazo.push({ numero, palo });
       }
     }
+  }
+
+  mezclarMazo(){
+    this.mazo.sort(()=> Math.random() - 0.5);
+  }
+
+  getNextCard(){
+    if(this.mazo.length > 0){
+      this.currentCard = this.nextCard;
+      this.nextCard = this.mazo.pop();
+      
+    }else{
+      console.log('No hay más cartas');
+    }
+  }
+
+  getCardImage(card : Card) : string{
+    return "/8bit-cards/"+card.numero.toString()+"-"+card.palo+".png";
+  }
 }
