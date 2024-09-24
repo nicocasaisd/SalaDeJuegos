d8f2375e6012e89b173c52d0efeb0528ecfddc8d
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-24 14:21:27 -0300

    funciona ahorcado

--- src/app/modules/juegos/ahorcado/ahorcado.component.ts
+++ src/app/modules/juegos/ahorcado/ahorcado.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BtnSalirComponent } from '../btn-salir/btn-salir.component';
 import { JuegosService } from '../../../services/juegos.service';
 import { CommonModule } from '@angular/common';
-import { initializeAnalytics } from '@angular/fire/analytics';
 
 @Component({
   selector: 'app-ahorcado',
@@ -26,6 +25,19 @@ export class AhorcadoComponent implements OnInit, OnDestroy {
   wordLength!: number;
   revealedLetters: boolean[] = [];
 
+  partesDelAhorcado = ['rope', 'head', 'body', 'left-arm', 'right-arm', 'left-leg', 'right-leg'];
+  cantidadDeErrores : number = 0;
+
+  gameEnded = false;
+
+  drawHangman(){
+    const part = document.getElementById(this.partesDelAhorcado[this.cantidadDeErrores]);
+    // Chequeamos si vuelve un elemento
+    if(part){
+      part.style.visibility = 'visible';
+    }
+  }
+
   getRandomWord() {
     const index = Math.floor(Math.random() * this.palabras.length);
     return this.palabras[index];
@@ -43,6 +55,7 @@ export class AhorcadoComponent implements OnInit, OnDestroy {
     this.wordLength = this.currentWord.length;
     // Inicializamos array de booleanos
     this.revealedLetters = new Array(this.currentWord.length).fill(false);
+    this.revealedLetters[1] = true; // borar
     console.log(this.currentWordList);
     console.log(this.revealedLetters);
   }
@@ -51,14 +64,34 @@ export class AhorcadoComponent implements OnInit, OnDestroy {
     // Chequeamos si la letra existe
     letter = letter.toUpperCase();
     console.log(letter);
+
+    let letterFound = false;
     
     for (let i = 0; i < this.currentWordList.length; i++) {
       console.log(this.currentWordList[i]);
       if (letter === this.currentWordList[i]) {
         this.revealedLetters[i] = true;
+        letterFound = true;
       }
     }
+    if(!letterFound){
+      this.drawHangman();
+      this.cantidadDeErrores ++;
+
+    }
     console.log(this.revealedLetters);
+    // Deshabilitamos el boton de esa letra
+    this.disableButton(letter);
+
+  }
+
+  disableButton(letter : string){
+    const button = document.getElementById('letra-'+letter.toLowerCase()) as HTMLButtonElement;
+    // Chequeamos si vuelve un elemento
+    if(button){
+      button.disabled = true;
+      button.style.backgroundColor = 'grey';
+    }
   }
 
   ngOnInit(): void {
