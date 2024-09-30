ee763e348fa01e3f45380905b61a9c0650af140c
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-09-30 18:27:25 -0300

    agrego servicios basicos y fondo

--- src/app/modules/juegos/juegos-routing.module.ts
+++ src/app/modules/juegos/juegos-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MayorMenorComponent } from './mayor-menor/mayor-menor.component';
 import { AhorcadoComponent } from './ahorcado/ahorcado.component';
 import { PreguntadosComponent } from './preguntados/preguntados.component';
+import { WordleComponent } from './wordle/wordle.component';
 
 const routes: Routes = [
   {path: 'mayor-menor', component: MayorMenorComponent},
   {path: 'ahorcado', component: AhorcadoComponent},
   {path: 'preguntados', component: PreguntadosComponent},
+  {path: 'wordle', component: WordleComponent},
 ];
 
 @NgModule({
--- src/app/modules/juegos/wordle/wordle.component.ts
+++ src/app/modules/juegos/wordle/wordle.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { BtnSalirComponent } from '../btn-salir/btn-salir.component';
+import { CommonModule } from '@angular/common';
+import { JuegosService } from '../../../services/juegos.service';
 
 @Component({
   selector: 'app-wordle',
   standalone: true,
-  imports: [],
+  imports: [BtnSalirComponent, CommonModule],
   templateUrl: './wordle.component.html',
   styleUrl: './wordle.component.css'
 })
 export class WordleComponent {
+  constructor(public juego: JuegosService){
+    
+  }
 
+  ngOnInit(): void {
+    this.juego.setModuleLoaded(true);
+    console.log('Se crea el componente');
+  }
+
+  ngOnDestroy(): void {
+    this.juego.setModuleLoaded(false);
+    console.log('Se destruye el componente');
+  }
 }
