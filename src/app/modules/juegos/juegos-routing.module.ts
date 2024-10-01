055be920969f2b31387c2ef7c452d0a02c9f6026
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-10-01 20:31:10 -0300

    agrego lazy loading de comps juegos

--- src/app/modules/juegos/juegos-routing.module.ts
+++ src/app/modules/juegos/juegos-routing.module.ts
@@ -6,10 +6,14 @@ import { PreguntadosComponent } from './preguntados/preguntados.component';
 import { WordleComponent } from './wordle/wordle.component';
 
 const routes: Routes = [
-  {path: 'mayor-menor', component: MayorMenorComponent},
-  {path: 'ahorcado', component: AhorcadoComponent},
-  {path: 'preguntados', component: PreguntadosComponent},
-  {path: 'wordle', component: WordleComponent},
+  {path: 'mayor-menor', loadComponent: ()=> import('./mayor-menor/mayor-menor.component').then(m=>m.MayorMenorComponent)},
+  //{path: 'mayor-menor', component: MayorMenorComponent},
+  {path: 'ahorcado', loadComponent: ()=> import('./ahorcado/ahorcado.component').then(m=>m.AhorcadoComponent)},
+  //{path: 'ahorcado', component: AhorcadoComponent},
+  {path: 'preguntados', loadComponent: ()=> import('./preguntados/preguntados.component').then(m=>m.PreguntadosComponent)},
+  //{path: 'preguntados', component: PreguntadosComponent},
+  {path: 'wordle', loadComponent: ()=> import('./wordle/wordle.component').then(m=>m.WordleComponent)},
+  //{path: 'wordle', component: WordleComponent},
 ];
 
 @NgModule({
