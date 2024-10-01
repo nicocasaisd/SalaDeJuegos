d965ab1c7999de49f880867ff9e7e03926f881d2
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-10-01 14:45:17 -0300

    agrego getPaises

--- src/app/services/preguntados.service.ts
+++ src/app/services/preguntados.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreguntadosService {
 
+  http = inject(HttpClient);
+  
   constructor() { }
+
+  getPaises() {
+    return this.http.get<any[]>('https://restcountries.com/v3.1/all')
+    .pipe(map(paises => paises.sort((a,b) => {
+        if (a.name.common < b.name.common) {
+          return -1;
+        } else if (a.name.common > b.name.common) {
+          return 1;
+        } else {
+          return 0;
+        }
+      })));
+  }
 }
