07a462bd2b24e8b8effd95c26684fa54072fa6e9
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-10-08 16:44:34 -0300

    completo formulario encuesta

--- src/app/components/encuesta/encuesta.component.ts
+++ src/app/components/encuesta/encuesta.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -13,43 +13,62 @@ import { FormsModule } from '@angular/forms';
 export class EncuestaComponent implements OnInit{
 
   form!: FormGroup;
+  options = ['Vista', 'Jugabilidad', 'UI/UX']; 
 
   ngOnInit(): void {
     this.form = new FormGroup({
 
-      nombre: new FormControl('', Validators.required),
-      apellido: new FormControl('', Validators.required),
+      nombre: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
+      apellido: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
       edad: new FormControl('', [Validators.required, Validators.min(18), Validators.max(99)]),
-      nro_telefono: new FormControl('', Validators.required),
-      question1: new FormControl('', [Validators.required]), // For radiobuttons
-      question2: new FormControl('', [Validators.required]), // For textbox
-      question3: new FormControl('', [Validators.required])  // For checkbox
+      nro_telefono: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.maxLength(10)]),
+      pregunta1: new FormControl('', [Validators.required]), // radiobuttons
+      pregunta2: new FormControl('', [Validators.required]), // textbox
+      pregunta3: new FormArray([])  // checkbox
 
     }
     );
+    this.addCheckboxes();
   }
 
-  get usuario() {
-    return this.form.get('usuario');
+  private addCheckboxes() {
+    this.options.forEach(() => this.pregunta3.push(new FormControl(false)));
   }
+
+  
+
   get nombre() {
     return this.form.get('nombre');
+  }
+  get apellido() {
+    return this.form.get('apellido');
   }
   get edad() {
     return this.form.get('edad');
   }
-  get mail() {
-    return this.form.get('mail');
+  get nro_telefono() {
+    return this.form.get('nro_telefono');
   }
-  get clave() {
-    return this.form.get('clave');
+  get pregunta1() {
+    return this.form.get('pregunta1');
   }
-  get repiteClave() {
-    return this.form.get('repiteClave');
+  get pregunta2() {
+    return this.form.get('pregunta2');
   }
+  get pregunta3() {
+    return this.form.get('pregunta3') as FormArray;
+  }
+
 
   enviarForm() {
     console.log(this.form.value);
-    
+    if (this.form.valid) {
+      const opcionesSeleccionadas = this.form.value.pregunta3
+        .map((checked: boolean, i: number) => checked ? this.options[i] : null)
+        .filter((v: string | null) => v !== null);
+      console.log('Formulario enviado correctamente', { ...this.form.value, opcionesSeleccionadas });
+    } else {
+      console.log('El formulario es invalido');
+    }
   }
 }
