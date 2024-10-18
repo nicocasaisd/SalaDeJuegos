64f69dffa39beafe98dbeca5e59e87db092caaed
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-10-18 17:24:37 -0300

    agrego puntajes component

---
+++ src/app/components/puntajes/puntajes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-puntajes',
+  standalone: true,
+  imports: [],
+  templateUrl: './puntajes.component.html',
+  styleUrl: './puntajes.component.css'
+})
+export class PuntajesComponent {
+
+}
